4980d7d0bfddab659ac0623fd74254a0c09be65f
Brett McMillin <brettmcm@me.com>
2024-08-03 15:29:41 -0700

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
       <section className={styles.hero}>
         <video loop={true} muted={true} autoPlay={true} playsInline={true} controls={false} className={styles.bg}>         
-            <source src="leaves.mp4" type="video/mp4"/>       
+            <source src="pattern.mp4" type="video/mp4"/>       
         </video>
 
         <h1>Innovative design crafting intuitive, inspiring experiences with precision and passion</h1>
@@ -18,7 +18,6 @@ export default function Home() {
       </section>
 
       <section className={`${styles.split} ${styles.flipped}`}>
-        {/* <img src="logo-animation.png" /> */}
         <video loop={true} muted={true} autoPlay={true} playsInline={true} controls={false} className={styles.half}>         
             <source src="mcm-intro.mp4" type="video/mp4"/>       
         </video>
@@ -38,7 +37,7 @@ export default function Home() {
 
       <section className={styles.disrupt}>
         <video loop={true} muted={true} autoPlay={true} playsInline={true} controls={false} className={styles.half}>         
-            <source src="pattern.mp4" type="video/mp4"/>       
+            <source src="pattern2.mp4" type="video/mp4"/>       
         </video>
       </section>
 
@@ -49,7 +48,7 @@ export default function Home() {
         <div className={styles.case}>
           <aside>
             <h4>Dusty Times</h4>
-            <p>Always learning and expanding, stretching into new areas to remain continuously challenged and inspired.</p>
+            <p>Embrace the thrill of off-road with Dusty Times—an annual lifestyle journal celebrating the world of desert racing and overlanding. Spotlighting the spirit of adventure, resilience, and the relentless pursuit of victory.</p>
             <ul aria-label="Scope of work"> 
                 <li>Design direction</li>
                 <li>Layout design</li>
@@ -95,7 +94,7 @@ export default function Home() {
         <div className={styles.case}>
           <aside>
             <h4>Mocks</h4>
-            <p>Always learning and expanding, stretching into new areas to remain continuously challenged and inspired.</p>
+            <p>A fast and intuitive mobile device mockup maker for mobile makers.</p>
             <ul aria-label="Scope of work"> 
                 <li>Product design</li>
                 <li><span className="proper">SwiftUI</span> development</li>
