6ac07a3c21a1c5ab22fcb251ec4c65010fe72b97
Brett McMillin <brettmcm@me.com>
2024-09-03 22:41:23 -0700

    removed icon include

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import styles from './styles/layout.module.scss'
-import Icon from "./icon";
 
 const inter = Inter({ subsets: ["latin"] });
 
